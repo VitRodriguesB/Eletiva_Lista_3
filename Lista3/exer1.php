6ba4cd11349a6df9d9282966bde1e1428cbaff80
Vitor Rodrigues Barbosa <vitornety2098@gmail.com>
2025-03-16 17:01:40 -0300

    exer1

--- Lista3/exer1.php
+++ Lista3/exer1.php
@@ -9,7 +9,7 @@
   <body>
   <div class = "container mt-5">
   <h1>Lista 3 Exercicio 1</h1>
-
+    
     <form action="exer1.php" method="POST">
         <?php for($i=0;$i<7;$i++): ?>
             <input type="number" name="num[]" placeholder="Numero"/>
